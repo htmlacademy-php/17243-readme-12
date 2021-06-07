00a150f90dc88b0b0a7472e0ee4b72884bed7209
Andrey Karamnoff <karamnoff@gmail.com>
2021-06-07 10:58:49 +0300

    Rename sql connection in init

--- config/init.php
+++ config/init.php
@@ -2,12 +2,12 @@
 require_once('./helpers.php');
 $db = require_once('config/db.php');
 
-$link = mysqli_connect($db['host'], $db['user'], $db['password'], $db['database'], $db['port']);
-if (!$link) {
+$con = mysqli_connect($db['host'], $db['user'], $db['password'], $db['database'], $db['port']);
+if (!$con) {
     echo "Cannot connect to database";
     die();
 }
 
-mysqli_set_charset($link, "utf8");
+mysqli_set_charset($con, "utf8");
 
 $is_auth = rand(0, 1);
